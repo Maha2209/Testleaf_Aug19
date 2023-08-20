42063673e2c9c9fc5cc1fbf5399dc08c0f2f4893
Maha2209 <142718601+Maha2209@users.noreply.github.com>
2023-08-20 15:55:25 +0530

    Add files via upload

---
+++ date1/HelloWorld.java
@@ -0,0 +1,12 @@
+package week1.date1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+	}
+	
+	
+
+}
---
+++ date1/Informations.java
@@ -0,0 +1,20 @@
+package week1.date1;
+
+public class Informations {
+	public static void main(String[] args) {
+		int age = 25;
+		long creditNum = 123456778759L;
+		float mileage = 20.5f;
+		boolean hadLunch = true;
+		double bankBalance = 6542.123456;
+		char logo = 'H';
+		String brandName = null;
+		System.out.println("Age of a customer is: " + age);
+		System.out.println("Credit Number of a customer is: " + creditNum);
+		System.out.println("Mileage: " + mileage);
+		System.out.println("boolean statment: " + hadLunch);
+		System.out.println("Bank balanace of a customer is: " + bankBalance);
+		System.out.println("Car logo: " + logo);
+		System.out.println("Car brand name is: " + brandName);
+	}
+}
